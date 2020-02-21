1ee25cd6f16f87445f4159f0cdc2eab4c52544e1
ziadalbana <58531158+ziadalbana@users.noreply.github.com>
2020-02-21 22:15:53 +0200

    Update ICalculator.java

--- ICalculator.java
+++ ICalculator.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package assigment.pkg1;
 
-/**
- *
- * @author Alex
- */
+package assigment.pkg1;
 public interface ICalculator {
     public int add(int x,int y);
     public float divide(int x,int y);    
